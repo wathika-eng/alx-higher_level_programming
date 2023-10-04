50a473b77e44769a73e7bc487c9944c4294e4ef6
wathika-eng <wathika02@gmail.com>
2023-10-05 00:16:58 +0300

    format fixes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for a in range(0, 100):
-    print(f"{a}", end=", ")
+    print("{}".format(a), end=", ")
     if a == 99:
-        print(f"{a}")
+        print("{}".format(a))
