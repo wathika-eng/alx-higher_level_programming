a9a4520501c1f02efa37503f52913e1ad310a18e
wathika-eng <wathika02@gmail.com>
2023-10-05 00:23:52 +0300

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for a in range(0, 10):
     for b in range(a + 1, 10):
         if a != 8 and b != 9:
-            print("{}{}".format(a,b), end=", ")
+            print("{}{}".format(a, b), end=", ")
         elif a == 8 and b == 9:
-            print("{}{}".format(a,b))
+            print("{}{}".format(a, b))
             break
