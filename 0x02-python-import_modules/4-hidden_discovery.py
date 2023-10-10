e6c6f3dd5b888db3be4de2cde2da4955331aed8e
wathika-eng <wathika02@gmail.com>
2023-10-10 20:06:55 +0300

    fixes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     toprint = dir(hidden_4)
 
     for name in toprint:
-        if name == "__":
+        if name[:2] == "__":
             print(toprint)
             break
