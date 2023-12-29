57715239057c5916e0d259a570366b9ea3789952
wathika-eng <wathika02@gmail.com>
2023-12-29 10:11:28 +0300

    ch

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,6 +50,15 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        if self.width or self.height == 0:
-            self.__perimeter = 0
-        return self.width + self.height
+        return 0 if self.width == 0 or self.height == 0 else (
+            self.width + self.height) * 2
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for i in range(self.height):
+            rectangle_str += "#" * self.width
+            if i != self.height - 1:
+                rectangle_str += "\n"
+        return rectangle_str[::-1]
+
