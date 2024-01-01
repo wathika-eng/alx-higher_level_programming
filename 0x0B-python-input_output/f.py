df0000285508a2a7cc12e313e3daf73f6119758c
wathika-eng <wathika02@gmail.com>
2024-01-01 10:24:16 +0300

    io

---
+++ 0x0B-python-input_output/f.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+
+import os
+
+with open("me.txt", mode="w", encoding="utf-8") as f:
+    f.write("Hello, World!\n")
+
+with open("me.txt", mode="r", encoding="utf-8") as f:
+    print (f.readline())
+
+
