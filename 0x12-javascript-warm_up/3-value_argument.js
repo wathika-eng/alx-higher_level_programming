a6ab89e5bd552953e6ecde03707c41501c8a01c7
wathika-eng <wathika02@gmail.com>
2024-01-08 22:59:36 +0300

    fn

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,11 +3,12 @@
   * If no arguments are passed to the script, print “No argument”
   * You must use console.log(...) to print all output
   * You are not allowed to use var
+  * You are not allowed to use length
  */
 
 const args = process.argv.slice(2);
-if (args.length === 0) {
+if (args[0] === undefined) {
   console.log('No argument');
-} else if (args.length >= 1) {
+} else {
   console.log(args[0]);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
-const dict = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const dict = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in dict) {
   console.log(dict[i]);
 }
