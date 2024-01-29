dc1615a27e389274275f66dc0da86473128b184b
wathika-eng <wathika02@gmail.com>
2024-01-29 14:06:46 +0300

    table

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates the table force_name on your MySQL server.
+CREATE TABLE force_name (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
