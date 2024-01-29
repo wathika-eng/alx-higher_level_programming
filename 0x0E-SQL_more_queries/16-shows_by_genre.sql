876343006aacff9db443c980bc65fcf10e223f0c
wathika-eng <wathika02@gmail.com>
2024-01-29 20:56:33 +0300

    final

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,14 +1,14 @@
--- Results are sorted in ascending order by the show title
+-- Results are sorted in ascending order by the show title and genre name.
 USE hbtn_0d_tvshows;
 SELECT
-    tv_shows.title
+    tv_shows.title,
+    tv_genres.name AS genre
 FROM
     hbtn_0d_tvshows.tv_shows
-JOIN
+LEFT JOIN
     hbtn_0d_tvshows.show_genres ON tv_shows.id = show_genres.show_id
-JOIN
+LEFT JOIN
     hbtn_0d_tvshows.tv_genres ON show_genres.genre_id = tv_genres.id
-WHERE
-    tv_genres.name = 'Comedy'
 ORDER BY
-    tv_shows.title ASC;
+    tv_shows.title ASC,
+    genre ASC;
