f3afb340d934f0d9ac38bcfb41c330136a2cdd50
wathika-eng <wathika02@gmail.com>
2024-02-02 17:35:51 +0300

    search

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Search the DB"""
+from sqlalchemy import create_engine
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    search_query = sys.argv[4]
+
+    engine = create_engine(
+        f"mysql://{user}:{password}@localhost:3306/{db}", pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    states = session.query(State).filter(State.name.like(f"%{search_query}%")).first()
+
+    if not states:
+        print("Nothing")
+    else:
+
+        print(f"{states.id}")
+
+    session.close()
