aaeb441119697840aa31631cd651402099cf291f
wathika-eng <wathika02@gmail.com>
2024-02-02 10:18:04 +0300

    syntaxfix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,7 @@
--- Write a script that creates the table force_name on your MySQL server.
-CREATE TABLE force_name (
+-- Write a script that creates the table force_name 
+-- on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
