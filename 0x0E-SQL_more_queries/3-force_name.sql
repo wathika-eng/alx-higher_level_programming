51fdbbbc81f75b6b06d00dec61f3bc54c17324b9
wathika-eng <wathika02@gmail.com>
2024-02-02 10:22:27 +0300

    varchar

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,6 +2,6 @@
 -- on your MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
